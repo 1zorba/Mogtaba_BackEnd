0011a20b247c4d1e819b15e2018a7f3ccf070a46
محمد حميد الشرعبي <2003mohammedalsharabi@gmail.com>
2026-05-01 23:34:25 +0300

    Execute migrations on startup

--- config/cors.php
+++ config/cors.php
@@ -28,15 +28,8 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
     'allowed_origins' => ['*'],
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // هذا السطر هو الأهم لحل المشكلة فوراً
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => true,
+   
 ];
