3df7bbfb4a447710f5b8f9ac4f6eddc1f2a83e8f
محمد حميد الشرعبي <2003mohammedalsharabi@gmail.com>
2026-05-01 23:38:58 +0300

    Execute migrations on startup

--- public/index.php
+++ public/index.php
@@ -3,13 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit;
-}
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
