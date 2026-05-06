310df2e626b0e4011f1f50c2018712c005c1f9d1
محمد حميد الشرعبي <2003mohammedalsharabi@gmail.com>
2026-05-06 21:46:00 +0300

    Fix API BASE_URL to production

--- config/cors.php
+++ config/cors.php
@@ -10,6 +10,7 @@ return [
         // 'http://localhost:5173', 
         // للتطوير المحلي
         'allowed_origins' => ['*'],
+        'https://mogtab.netlify.app',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
