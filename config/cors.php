253d8e3fb22addbfbfe3c6b88c1ff5edd1ae01a6
محمد حميد الشرعبي <2003mohammedalsharabi@gmail.com>
2026-05-06 21:51:15 +0300

    Fix API BASE_URL to production

--- config/cors.php
+++ config/cors.php
@@ -2,19 +2,22 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    // هنا نضع الروابط المحددة فقط ونحذف النجمة نهائياً
+
+    // ركز هنا: تأكد أنها مصفوفة تحتوي على نصوص
     'allowed_origins' => [
-        // 'https://mogtaba-front-end.vercel.app/',
-        // 'https://mogtaba-front-end-1102mohammed-8454s-projects.vercel.app', // الرابط الذي يظهر في الخطأ عندك
-        // 'http://localhost:5173', 
-        // للتطوير المحلي
-        'allowed_origins' => ['*'],
-        'allowed_origins' => ['https://mogtab.netlify.app'], // تأكد من عدم وجود / في النهاية
+        'https://mogtab.netlify.app',
+        'https://mogtaba-front-end-1102mohammed-8454s-projects.vercel.app'
     ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => true, // ضرورية لأنك تستخدم Sanctum
+
+    'supports_credentials' => true, // اجعلها true إذا كنت تستخدم Login/Authm
 ];
