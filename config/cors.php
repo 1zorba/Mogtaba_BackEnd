3db10360970cd9e98d99542256676cd19c793ff4
محمد حميد الشرعبي <2003mohammedalsharabi@gmail.com>
2026-05-13 23:56:47 +0300

    fix: update storage URL logic and env variables

--- config/cors.php
+++ config/cors.php
@@ -5,10 +5,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    // ركز هنا: تأكد أنها مصفوفة تحتوي على نصوص
+    // ضع هنا رابط موقعك على Netlify بدقة
     'allowed_origins' => [
         'https://mogtab.netlify.app',
-        'https://mogtaba-front-end-1102mohammed-8454s-projects.vercel.app',
+        'http://localhost:5173', // للسماح بالتجربة المحلية أيضاً
     ],
 
     'allowed_origins_patterns' => [],
@@ -19,5 +19,32 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true, // اجعلها true إذا كنت تستخدم Login/Authm
+    // اجعلها true إذا كنت تستخدم نظام تسجيل دخول (Cookies/Sanctum)
+    'supports_credentials' => true,
 ];
+
+
+
+
+
+// return [
+//     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+//     'allowed_methods' => ['*'],
+
+//     // ركز هنا: تأكد أنها مصفوفة تحتوي على نصوص
+//     'allowed_origins' => [
+//         'https://mogtab.netlify.app',
+//         'https://mogtaba-front-end-1102mohammed-8454s-projects.vercel.app',
+//     ],
+
+//     'allowed_origins_patterns' => [],
+
+//     'allowed_headers' => ['*'],
+
+//     'exposed_headers' => [],
+
+//     'max_age' => 0,
+
+//     'supports_credentials' => true, // اجعلها true إذا كنت تستخدم Login/Authm
+// ];
